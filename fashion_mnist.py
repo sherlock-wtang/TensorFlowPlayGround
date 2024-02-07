9c60bb1d23849f752779f222cae1db1fc773af6d
Sherlock Tang <sherlockshin@outlook.jp>
2024-02-07 23:49:17 +0900

    new file

---
+++ fashion_mnist.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(f'Tensorflow Version:{tf.__version__}\n')
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_lables), (test_images, test_lables) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# print(f"{train_images.shape}")
+# plt.figure()
+# plt.imshow(train_images[1])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+train_images = train_images/255
+test_images = test_images/255
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_lables[i]])
+plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer = 'adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentrypy(from_logits=True),
+              metrics=['accuracy'])
+
